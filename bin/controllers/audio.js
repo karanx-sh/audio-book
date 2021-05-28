009bc335db9b60a14cdd961dfba5d6fd365d40a1
kphacko <25796369+kphacko@users.noreply.github.com>
2021-05-28 11:42:13 +0530

    test changes 3

--- bin/controllers/audio.js
+++ bin/controllers/audio.js
@@ -7,11 +7,12 @@ const CustomError = require("../custom/error"); // Importing Custome Error class
 const customError = require("../custom/errors"); // Importing Developer Defined Custom Errors
 const { getSignedURL, uploadFile, deleteFile } = require("../custom/s3");
 const cloud = require("../custom/cloud");
+const rootPath = require("../../root_path");
 
 const audioStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log(path.join("uploads"));
-    cb(null, path.join("uploads")); // save the initial in uploads folder
+    console.log(path.join(rootPath, "uploads"));
+    cb(null, path.join(rootPath, "uploads")); // save the initial in uploads folder
   },
   filename: function (req, file, cb) {
     console.log("Disk storage  " + file.originalname);
---
+++ root_path.js
@@ -0,0 +1,4 @@
+const path = require("path");
+const projectRootPath = path.resolve(__dirname);
+console.log(projectRootPath);
+module.exports = projectRootPath;
